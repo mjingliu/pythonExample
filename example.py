93f4a6e4f6355635d978090976212313b3c29fa9
liumingjing@microknown.com <liumingjing@microknown.com>
2020-06-23 22:44:14 +0800

    fetch the whole daily data from Tushare

--- example.py
+++ example.py
@@ -10,74 +10,81 @@ import pandas as pd
 import tushare as ts
 import const
 import sys, os
+import time
+import datetime
 
-# df = ts.pro_api("cf8c206c8a3f3919835effcfc672c48a7f3b40315a1867bd15375af5")
-
-url = "http://api.waditu.com"
-timeout = 15
-headers = {'Connection': 'close'}
-proxies = {"http": "http://10.144.1.10:8080"}
-
-req_para = {
-    'api_name': 'daily',
-    'token': 'cf8c206c8a3f3919835effcfc672c48a7f3b40315a1867bd15375af5',
-    'params': {'ts_code': '002415.SZ', 'start_date': '19950101', 'end_date': '20200601'},
-    'fields': ''
-}
-#海康：002415.SZ
-#工行：601398.SH
-#东方通信：600776.SH
-#茅台：600519.SH
 '''
-req_para = {
-    'api_name': 'daily',
-    'token': 'cf8c206c8a3f3919835effcfc672c48a7f3b40315a1867bd15375af5',
-    'params': {'exchange': '', 'list_status': 'L'},
-    'fields': ''
-}
+1. 每一次抓取的数据存放在../tmpData/目录下，故当前要工作在../data/目录下
 '''
-tmpDir=os.getcwd()
-tmpDataDir = ''.join(r'\tmpData')
-print('\n')
-print(tmpDataDir)
-print('\n')
-tmpDataDir ='257'.join(r'\Data')
-print(tmpDataDir)
-
-tmpDir = os.getcwd()
+tmpDir, tmpFile = os.path.split(sys.argv[0])
 tmpDataDir = tmpDir + r'\tmpData'
 if not os.path.exists(tmpDataDir):
     os.mkdir(tmpDataDir)
 os.chdir(tmpDataDir)
 print(os.getcwd())
-os.chdir(tmpDir)
-print(os.getcwd())
 print("\n sys.argv")
-#print(sys.argv[0])
-tmpDir, tmpFile = os.path.split(sys.argv[0])
-print("tmpDir:%s" % tmpDir)
-print("tmpFile %s" % tmpFile)
 
-#result = req.post(url=url, json=req_para, timeout=timeout, headers=headers, proxies=proxies)
-result = req.post(url=url, json=req_para, timeout=timeout, headers=headers)
-if result:
-    res = json.loads(result.text)
-    #print(res)
-    if res['code'] != 0:
-        print("Fail to fetch data: %s" % res['msg'])
-    else:
-        data = res['data']
-        col = data['fields']
-        record = data['items']
-        #print(col)
-        #print(record)
-        df = pd.DataFrame(data=record, columns=col)
-        print(df)
-        df.to_csv("data.csv", columns=col, header=False,index= False)
-        df.to_csv("data1.csv",index=False)
-else:
-    print('\n')
-    print("failed to fetch data this time!\n")
+url = "http://api.waditu.com"
+timeout = 15
+headers = {'Connection': 'close'}
+# 当适用公司网络的时候，需要适用如下proxies，在家里的网络不需要适用proxies
+# proxies = {"http": "http://10.144.1.10:8080"}
+
+td = datetime.date.today().strftime("%Y%m%d")
+# tdStr = datetime.datetime.strptime(td, "%Y%m%d")
+# td = datetime.datetime.today().strftime("%Y%m%d")
+
+cNum = 0
+
+for eachCode in const.stockCode:
+    '''
+    eachCode: 每一支股票的在深市/沪市的编号
+    const.stockCode[eachCode]: 获取到每一支股票在Tushare中的映射
+    '''
+    # cNum += 1
+    time.sleep(1) # 1 second取一次数据，用以规避频繁取数据，被服务器禁止服务
+    stckCode = const.stockCode[eachCode]
+    filename = td + '_' + stckCode + '.csv'
+    #if cNum <= 10:
+    req_para = {
+        'api_name' : 'daily',
+        'token' : 'cf8c206c8a3f3919835effcfc672c48a7f3b40315a1867bd15375af5',
+        'params' : {'ts_code' : ''.join(stckCode), 'start_date' : '19950101', 'end_date' : ''.join(td)},
+        'fields' : ''
+    }
+    # 海康：002415.SZ
+    # 工行：601398.SH
+    # 东方通信：600776.SH
+    # 茅台：600519.SH
+    '''
+    req_para = {
+        'api_name': 'daily',
+        'token': 'cf8c206c8a3f3919835effcfc672c48a7f3b40315a1867bd15375af5',
+        'params': {'exchange': '', 'list_status': 'L'},
+        'fields': ''
+    }
+    '''
+    # result = req.post(url=url, json=req_para, timeout=timeout, headers=headers, proxies=proxies)
+    try :
+        result = req.post(url=url, json=req_para, timeout=timeout, headers=headers)
+    except Exception as e :
+        print("exception take place: code:%s" % stckCode)
+        print("exception is :%s" % e)
+    else :
+        if result :
+            res = json.loads(result.text)
+            if res['code'] != 0 :
+                print("Fail to fetch data: %s" % res['msg'])
+            else :
+                data = res['data']
+                col = data['fields']
+                record = data['items']
+                df = pd.DataFrame(data=record, columns=col)
+                # print(df)
+                df.to_csv("".join(filename), columns=col, header=False, index=False)
+        else :
+            print('\n')
+            print("failed to fetch data this time!\n")
 
 '''
 股票精选步骤：
@@ -86,6 +93,7 @@ else:
 3. 对存储到MySQL中的数据进行加工，根据自己的想法，可以采用FFT变换，看一下在另一个空间中的数据是什么样子的？
 4. 结合当下的历史数据分析，筛选出对应的行业，看看不同类型的行业中，是否有属于行业自己的特性
 5. 如果要自动化交易的话，需要根据历史数据进行有限的分析，具体该如何分析，暂时还不清楚
+    a. 从当前的实验情况来看，FFT/iFFT不适用该分析，
+    b. 
 6. 可以做一个可视化的界面，更有利于交易分析
 '''
-
