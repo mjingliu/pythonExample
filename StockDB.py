56976d92de747a14e5d1a1a7871a603270060e12
liumingjing@microknown.com <liumingjing@microknown.com>
2020-07-05 22:36:55 +0800

    implement class database

---
+++ StockDB.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python 
+# -*- coding:utf-8 -*-
+
+import pymysql as psql
+
+class stockDB(object):
+    '''
+    class stockDB: use this class to implement the specific DB access using pymysql lib
+    '''
+    def __init__(self):
+        pass
+    def login(self):
+        pass
+    def useDB(self):
+        pass
+    def selectData(self):
+        pass
+    def storeData(self):
+        pass
+    def exitDB(self):
+        pass
