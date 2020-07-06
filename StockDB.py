63ee29eed06f6d39dc189f2c0ce3283e2f3bae26
liumingjing@microknown.com <liumingjing@microknown.com>
2020-07-06 18:18:07 +0800

    update the local code

--- StockDB.py
+++ StockDB.py
@@ -2,6 +2,7 @@
 # -*- coding:utf-8 -*-
 
 import pymysql as psql
+import const
 
 class stockDB(object):
     '''
@@ -22,3 +23,9 @@ class stockDB(object):
     def exitDB(self):
         self.cursor.close()
         self.conn.close()
+
+if __name__ == '__main__':
+    myDB = stockDB()
+    for each in const.stockCode:
+        filename = '20200623' + '_' + ''.join(each) + '.csv'
+    pass
