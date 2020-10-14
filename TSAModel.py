86b8afb06889b9803a5ddf5fc794c0bdb9bbb1de
mjingliu <11746468@qq.com>
2020-10-14 18:19:14 +0800

    add the functionality of ARModel class

--- TSAModel.py
+++ TSAModel.py
@@ -11,18 +11,45 @@ class ARModel(object):
     output:
     '''
     def __init__(self, data):
-        self.data = data
+        '''
+        输入的数据应该是通过平稳性检测过的数据
+        '''
+        self.basicStat = tss.StatisticsModel(data)
 
     def Regression(self):
         # 回归
-        pass
+        '''
+        1. 阶数估计，ACF计算/PACF计算
+        2. 根据准则，确认阶数
+        3. 构造数据，进行最小二乘估计，获取系数以及对应的系数方差
+        notes:
+        AR(P), 主要是看PACF的结尾
+        MA(Q), 主要是看ACF的结尾
+        '''
+        iOrderLimit = self.basicStat.getOrder()
+        iACF = self.basicStat.getACF(iOrderLimit)
+        iPACF = self.basicStat.getPACF(iOrderLimit)
+
+        iThreshold = self.basicStat.getACFPACFCoefTest()
+
+        iACFIndex = self.__getOrderIdx__(iACF, iThreshold)
+        iPACFIndex = self.__getOrderIdx__(iPACF, iThreshold)
+
+        
+
+    def __getOrderIdx__(self, data, threshold):
+        iArr = []
+        for i in range(len(data)):
+            if abs(data[i]) > abs(threshold[0]):
+                iArr.append(i)
+        return iArr
 
     def ARTest(self):
         # AR检测
         pass
 
     def Estimation(self):
-        # 估计
+        # 估计，获取到对应的参数以及参数对应的方差
         pass
 
     def Prediction(self):
