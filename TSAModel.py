462718069df15dcbbf6cd9f9eabc1c0635853b3e
liumingjing@microknown.com <liumingjing@microknown.com>
2020-10-14 21:02:39 +0800

    update TFAModel.py

--- TSAModel.py
+++ TSAModel.py
@@ -3,17 +3,30 @@
 from builtins import object
 
 import TSASkills as tss
+import infrastructure as infra
 
 class ARModel(object):
-    def __init__(self):
-        pass
-
+    def __init__(self, data):
+        self.statBasic = tss.StatisticsModel(data)
+        
     def Regression(self):
         # 回归
         pass
 
     def ARTest(self):
         # AR检测
+        pass
+
+    def Errors(self):
+        # 误差
+        pass
+
+    def Estimation(self):
+        # 估计
+        pass
+
+    def Prediction(self):
+        # 预测
         pass
 
 class MAModel(object):
